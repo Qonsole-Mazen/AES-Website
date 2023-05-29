72fe5f87a4c839bba150d6133dda0d6cb5cd70a3
Qonsole-Mazen <qonsole.mazen@gmail.com>
2023-05-29 10:02:01 +0300

    --

--- web2/main.js
+++ web2/main.js
@@ -3,36 +3,39 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("slide-item");
+  let slides = document.getElementsByClassName('slide-item');
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-const openMenu = document.querySelector(".bx-menu")
-const closeMenu = document.querySelector(".bx-x")
-const menu = document.querySelector('.nav')
-closeMenu.style.display ='none'
-openMenu.addEventListener("click", (e) =>{
-    e.target.style.display = 'none'
-    closeMenu.style.display = 'inline-block'
-    menu.classList.toggle('hide')
-})
-closeMenu.addEventListener("click", (e) =>{
-    e.target.style.display = 'none'
-    openMenu.style.display = 'inline-block'
-    menu.classList.toggle('hide')
-})
+const openMenu = document.querySelector('.bx-menu');
+const closeMenu = document.querySelector('.bx-x');
+const menu = document.querySelector('.nav');
+closeMenu.style.display = 'none';
+openMenu.addEventListener('click', (e) => {
+  e.target.style.display = 'none';
+  closeMenu.style.display = 'inline-block';
+  menu.classList.toggle('hide');
+});
+closeMenu.addEventListener('click', (e) => {
+  e.target.style.display = 'none';
+  openMenu.style.display = 'inline-block';
+  menu.classList.toggle('hide');
+});
 
-const submnue = document.querySelectorAll('.sub')
-console.log(submnue[0].children[1])
-submnue.forEach(el=>{
-    el.addEventListener('click',e=>{
-    console.log(e.target.nextElementSibling )
-        e.target.nextElementSibling .classList.toggle('hide')
-    })
-})
+const submnue = document.querySelectorAll('.sub');
+submnue.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.target.classList.toggle('opened');
+    e.target.childNodes[1].classList.toggle('rot');
+    e.target.nextElementSibling.classList.toggle('hide');
+    e.target.nextElementSibling.style.transation = '.5s';
+  });
+});
